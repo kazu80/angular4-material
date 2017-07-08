350f4c9005d0aa575676322b43a1307b5bcffdca
monkick <saruyama.monki@gmail.com>
2017-07-08 15:44:23 +0900

    Toolbar & button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdCommonModule, MdMenuModule, MdToolbarModule} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-      MdButtonModule,
-      MdCheckboxModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdCheckboxModule,
+        MdMenuModule,
+        MdCommonModule,
+        MdToolbarModule
+    ],
+    providers   : [],
+    bootstrap   : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
